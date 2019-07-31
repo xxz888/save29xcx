396f898b87cd55a963efc87e2a38e320622757d1
xxz888 <6952731@qq.com>
2019-07-31 10:14:30 +0800

    first commit

---
+++ data/data_index.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var index= {
+    "id": 1,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca",
+            "feed_source_txt": "赞了回答1",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "What are the attractions in China？",
+            "answer_ctnt": "I think hangzhou is the most beautiful city.The west lake in hangzhou is beautiful and the people are friendly.Alibaba group is also in hangzhou.",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex",
+            "feed_source_txt": "回答了问题2",
+            "feed_source_img": "../../images/icon8.jpg",
+          "question": "Late to learn piano at 26？",
+          "answer_ctnt": "The best time to learn the piano is right now.At 20, it's a little late for you to become a professional pianist, but it's never too late to",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+          "question": "Late to learn piano at 26？",
+          "answer_ctnt": "The best time to learn the piano is right now.At 20, it's a little late for you to become a professional pianist, but it's never too late to",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+          {
+            "question_id": 4,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "Late to learn piano at 26？",
+            "answer_ctnt": "The best time to learn the piano is right now.At 20, it's a little late for you to become a professional pianist, but it's never too late to",
+            "good_num": "112",
+            "comment_num": "18"
+          },
+          {
+            "question_id": 5,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "Late to learn piano at 26？",
+            "answer_ctnt": "The best time to learn the piano is right now.At 20, it's a little late for you to become a professional pianist, but it's never too late to",
+            "good_num": "112",
+            "comment_num": "18"
+          },
+          {
+            "question_id": 6,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../../images/icon9.jpeg",
+            "question": "Late to learn piano at 26？",
+            "answer_ctnt": "The best time to learn the piano is right now.At 20, it's a little late for you to become a professional pianist, but it's never too late to",
+            "good_num": "112",
+            "comment_num": "18"
+          },
+    ]
+
+}
+
+module.exports.index = index;
---
+++ pages/Post/post.js
@@ -0,0 +1,90 @@
+// pages/Post/post.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    placeholderImg:"../../images/tabbar/添加图片.png",
+    photo1: "../../images/xxz/添加图片.png",
+    photo2: "../../images/xxz/添加图片.png",
+    photo3: "../../images/xxz/添加图片.png",
+
+  },
+  gotoShow:function(event){
+    var self = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths[0];
+        var index = event.currentTarget.dataset.index;
+        if(index == 1){
+          self.photo1 = tempFilePaths;
+        }else if(index == 2){
+          self.photo2 = tempFilePaths;
+        }else{
+          self.photo3 = tempFilePaths;
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   
+
+   sdfs
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
